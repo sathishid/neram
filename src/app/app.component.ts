8337949104491880a2d4e362208d69d41e0b1040
Sathish <sathish.addon@gmail.com>
2025-02-07 22:20:11 +0530

    HolyGrail Achiebved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
